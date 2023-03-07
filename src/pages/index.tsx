12f13cd17f4bfea905964eaa85edafdca27e9139
zo0r <z@zo0r.me>
2023-03-06 23:13:11 -0500

    update favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ export default function HomePage() {
       <Head>
         <title>Verza Scripts</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/img/favicon.png" />
       </Head>
 
       <div
