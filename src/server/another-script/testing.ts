5669abd8a9c5d744a6fed788caa4d3950c2286dc
zo0r <z@zo0r.me>
2023-10-04 02:24:09 -0400

    Test

--- src/server/another-script/testing.ts
+++
@@ -1,3 +0,0 @@
-export const calcIt = (a: number, b: number) => {
-  return a + b;
-};
--- src/server/script.ts
+++ src/server/script.ts
@@ -1,17 +1,15 @@
 import {Command, initEngine} from '@verza/sdk';
 import {CommandParam} from '@verza/sdk';
 
-const SERVER_TOKEN =
-  'f9d49e0fd45ed376ed4dec14bb78afb8b4e66a2333871b195a8c160a60f68eae';
+/* const SERVER_TOKEN =
+  'f9d49e0fd45ed376ed4dec14bb78afb8b4e66a2333871b195a8c160a60f68eae'; */
 
 export default async function script() {
-  //console.log(typeof process !== 'undefined' ? process.env : null);
-
   const engine = await initEngine({
     environment: 'dev',
-    apiEndpoint: 'http://localhost',
+    //apiEndpoint: 'http://localhost',
     name: 'Web Server',
-    accessToken: SERVER_TOKEN,
+    accessToken: process.env.SERVER_TOKEN,
   });
 
   // command
